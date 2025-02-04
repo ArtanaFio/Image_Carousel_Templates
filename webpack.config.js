1287809106562cd68bb11a5f6a3d8f099eee6a41
ArtanaFio <125154911+ArtanaFio@users.noreply.github.com>
2025-02-04 11:12:22 -0500

    perf: Use image-webpack-loader to optimize images
    Configured image-webpack-loader in webpack.config.js to compress images
    for better performance. This was intended to improve image rendering
    speed, but images still do not load fully at once when scrolling.
    Further investigation needed to address delayed rendering.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const ImageWebpackLoader = require("image-webpack-loader");
 
 module.exports = {
     mode: "development",
@@ -11,19 +13,26 @@ module.exports = {
     },
     devtool: "eval-source-map",
     devServer: {
+        static:path.resolve(__dirname, "dist"),
         watchFiles: ["./src/index.html"],
         open: true,
+        hot: true,
+        compress: true,
     },
     plugins: [
         new HtmlWebpackPlugin ({
             template: "./src/index.html",
+            scriptLoading: 'defer',
         }),
+        new MiniCssExtractPlugin({
+            filename: "styles.css",
+        })
     ],
     module: {
         rules: [
             {
                 test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
             },
             {
                 test: /\.html$/i,
@@ -31,7 +40,25 @@ module.exports = {
             },
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: "asset/resource",
+                type: 'asset/resource',
+                generator: {
+                    filename: 'images/[name][ext]',
+                },
+                use: [
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            mozjpeg: { 
+                                progressive: true,
+                                quality: 40 
+                            }, // compress JPGs
+                            svggo: {
+                                pluggins: [{ removeViewBox: false }] // ensures SVGs remail valid, don't remove viewBox
+                            },
+                            webp: { quality: 50 }, //convert images to WebP format, reduce WebP quality for smaller sizes
+                        },
+                    },
+                ],
             },
         ],
     },
